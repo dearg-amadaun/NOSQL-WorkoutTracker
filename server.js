71fea77d59ef52f9a530920dc6e35ea65278b54e
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-10-07 12:48:25 -0400

    added index.js to html routes file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const db = require("./config")
+const db = require("./config/connection")
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.use(express.static("public"));
